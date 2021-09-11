680d8def31b37a29929f389f4b3344b412c46fb1
unknown <vincentgosselin1@gmail.com>
2021-09-11 13:18:20 -0400

    test2 nice 8Mbps

--- tcp_ip_demo.tcl
+++ tcp_ip_demo.tcl
@@ -1,14 +1,60 @@
 #tcl program to test TCP IP.
 #Vincent Gosselin, 2021.
 
- set chan [socket 192.168.1.10 7]         ;# Open the connection
+ # set chan [socket 192.168.1.10 7]         ;# Open the connection
+  # set i 0
+ # while {$i < 100000000000000000} {
+     # puts $chan [expr $i * $i]                         ;# Send a string
+	 # flush $chan                              ;# Flush the output buffer
+	 # gets $chan line
+	 # #puts "received $line"
+	 # incr i
+# }
+# close $chan                              ;# Close the socket
 
- set i 0
+set long "i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx \
+i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx i ♥ u xilinx  "
+
+
+ set chan [socket 192.168.1.10 7]         ;# Open the connection
+  set i 0
  while {$i < 100000000000000000} {
-     puts $chan [expr $i * $i]                         ;# Send a string
+     puts $chan $long                        ;# Send a string
 	 flush $chan                              ;# Flush the output buffer
 	 gets $chan line
-	 puts "received $line"
+	 #puts "received $line"
 	 incr i
 }
 close $chan                              ;# Close the socket
